011b2cc1b9471933dbe0f5ef08c9119d3ee1c1f9
ParkSeYun <yun12343@naver.com>
2023-03-29 22:32:49 +0900

    dijkstra

--- BaekJoon/Gold/G4_1504_특정한_최단_경로.java
+++ BaekJoon/Gold/G4_1504_특정한_최단_경로.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class G5_1916_최소비용_구하기 {
+public class G4_1504_특정한_최단_경로 {
     private static int N;
-    private static int M;
-    private static int start;
-    private static int end;
+    private static int E;
+    private static int v1;
+    private static int v2;
 
-    private static boolean[] visited;
+    // 200000 * 1000
+    private static final int INF = 200000000;
+
     private static int[] dist;
 
     private static List<Node>[] graph;
@@ -21,52 +23,56 @@ public class G5_1916_최소비용_구하기 {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
+        dist = new int[N+1];
         graph = new ArrayList[N+1];
 
         for(int i=0; i<N+1; i++)
             graph[i] = new ArrayList<>();
 
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
+        for(int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            graph[s].add(new Node(e, w));
-
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
         }
 
         st = new StringTokenizer(br.readLine(), " ");
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+        v1 = Integer.parseInt(st.nextToken());
+        v2 = Integer.parseInt(st.nextToken());
 
-        int INF = Integer.MAX_VALUE;
+        int routeA = 0;
+        int routeB = 0;
 
-        dist = new int[N+1];
-        visited = new boolean[N+1];
+        // start -> v1 -> v2 -> end
+        routeA += Dijkstra(1, v1);
+        routeA += Dijkstra(v1, v2);
+        routeA += Dijkstra(v2, N);
+        // start -> v1 -> v2 -> end
+        routeB += Dijkstra(1, v2);
+        routeB += Dijkstra(v2, v1);
+        routeB += Dijkstra(v1, N);
 
+        if(routeA >= INF && routeB >= INF)
+            System.out.println(-1);
+        else
+            System.out.println(Math.min(routeA, routeB));
+    }
+
+    public static int Dijkstra(int start, int end) {
         Arrays.fill(dist, INF);
         dist[start] = 0;
 
-        Dijkstra();
-
-        System.out.println(dist[end]);
-    }
-
-    public static void Dijkstra() {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(start, 0));
 
         while(!pq.isEmpty()) {
             Node now = pq.poll();
-
-            if(visited[now.idx])
-                continue;
-
-            visited[now.idx] = true;
 
             for(Node next : graph[now.idx]) {
                 if(dist[next.idx] > dist[now.idx] + next.cost) {
@@ -75,6 +81,8 @@ public class G5_1916_최소비용_구하기 {
                 }
             }
         }
+
+        return dist[end];
     }
 
     public static class Node implements Comparable<Node> {
