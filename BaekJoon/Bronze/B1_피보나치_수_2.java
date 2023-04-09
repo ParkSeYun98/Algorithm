57f867e259cddc5dbca945bc9f721fa27bc72cd8
ParkSeYun <yun12343@naver.com>
2023-04-09 16:58:38 +0900

    DP

---
+++ BaekJoon/Bronze/B1_피보나치_수_2.java
@@ -0,0 +1,23 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1_피보나치_수_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n+1];
+
+        dp[0] = 0L;
+        dp[1] = 1L;
+
+        for(int i=2; i<dp.length; i++)
+            dp[i] = dp[i-2] + dp[i-1];
+
+        System.out.println(dp[n]);
+    }
+}
