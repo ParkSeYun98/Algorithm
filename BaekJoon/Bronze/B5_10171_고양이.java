475dc7b0e164d7a05e7b6353391a28e899d4857a
ParkSeYun <yun12343@naver.com>
2023-04-26 08:55:35 +0900

    0426

---
+++ BaekJoon/Bronze/B5_10171_고양이.java
@@ -0,0 +1,18 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B5_10171_고양이 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+
+        System.out.println(sb);
+    }
+}
