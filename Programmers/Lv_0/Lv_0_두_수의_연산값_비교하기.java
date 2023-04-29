c548f907f22b4433ed0fbb56d3c17537c5dfbcc2
ParkSeYun <yun12343@naver.com>
2023-04-29 12:57:31 +0900

    String, Integer

---
+++ Programmers/Lv_0/Lv_0_두_수의_연산값_비교하기.java
@@ -0,0 +1,18 @@
+package Programmers.Lv_0;
+
+public class Lv_0_두_수의_연산값_비교하기 {
+    class Solution {
+        public int solution(int a, int b) {
+            int answer = 0;
+
+            String str = "";
+
+            str += String.valueOf(a);
+            str += String.valueOf(b);
+
+            answer = Math.max(Integer.parseInt(str), 2*a*b);
+
+            return answer;
+        }
+    }
+}
