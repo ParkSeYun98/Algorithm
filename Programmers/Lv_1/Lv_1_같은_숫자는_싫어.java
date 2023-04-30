5ef2be5a76fbe441cd0e2fb281bb08cdcd81a1a0
ParkSeYun <yun12343@naver.com>
2023-04-30 16:50:35 +0900

    Stack

---
+++ Programmers/Lv_1/Lv_1_같은_숫자는_싫어.java
@@ -0,0 +1,29 @@
+package Programmers.Lv_1;
+
+import java.util.*;
+
+public class Lv_1_같은_숫자는_싫어 {
+    public class Solution {
+        public int[] solution(int []arr) {
+            int[] answer = {};
+
+            Stack<Integer> stack = new Stack<>();
+
+            for(int i=0; i<arr.length; i++) {
+                if(stack.size() == 0)
+                    stack.push(arr[i]);
+                else {
+                    if(stack.peek() != arr[i])
+                        stack.push(arr[i]);
+                }
+            }
+
+            answer = new int[stack.size()];
+
+            while(!stack.isEmpty())
+                answer[stack.size()-1] = stack.pop();
+
+            return answer;
+        }
+    }
+}
