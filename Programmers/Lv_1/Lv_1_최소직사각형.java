e6d9e72b3dbce3ffebf70d9472b7dcbfef51adb0
ParkSeYun <yun12343@naver.com>
2023-05-01 16:23:14 +0900

    thinking

---
+++ Programmers/Lv_1/Lv_1_최소직사각형.java
@@ -0,0 +1,36 @@
+package Programmers.Lv_1;
+
+public class Lv_1_최소직사각형 {
+    class Solution {
+        public int solution(int[][] sizes) {
+            int answer = 0;
+
+            for(int i=0; i<sizes.length; i++) {
+                if(sizes[i][0] < sizes[i][1])
+                    switching(i, sizes);
+            }
+
+            answer = calc(sizes);
+
+            return answer;
+        }
+
+        void switching(int idx, int[][] sizes) {
+            int temp = sizes[idx][0];
+            sizes[idx][0] = sizes[idx][1];
+            sizes[idx][1] = temp;
+        }
+
+        int calc(int[][] sizes) {
+            int maxWidth = -1;
+            int maxHeight = -1;
+
+            for(int i=0; i<sizes.length; i++) {
+                maxWidth = Math.max(maxWidth, sizes[i][0]);
+                maxHeight = Math.max(maxHeight, sizes[i][1]);
+            }
+
+            return maxWidth*maxHeight;
+        }
+    }
+}
