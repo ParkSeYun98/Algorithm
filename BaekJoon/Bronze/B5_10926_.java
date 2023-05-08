b1ad8f6d5629483de006e8b7d2d7af14cbb4a3ad
ParkSeYun <yun12343@naver.com>
2023-05-08 11:42:39 +0900

    입출력과 사칙연산

---
+++ BaekJoon/Bronze/B5_10926_.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_10926_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(input + "??!");
+    }
+}
