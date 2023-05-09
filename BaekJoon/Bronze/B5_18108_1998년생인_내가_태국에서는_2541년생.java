a9adfac10487b5bebe6e5a70e08161b4a0a57efd
ParkSeYun <yun12343@naver.com>
2023-05-09 09:10:50 +0900

    입출력과 사칙연산

---
+++ BaekJoon/Bronze/B5_18108_1998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_18108_1998년생인_내가_태국에서는_2541년생 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        System.out.println(input - 543);
+    }
+}
