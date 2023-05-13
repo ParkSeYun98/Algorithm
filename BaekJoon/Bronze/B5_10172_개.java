7f7abedf7d977a819ceebf4b43bd3907282bc1b6
ParkSeYun <yun12343@naver.com>
2023-05-13 12:49:00 +0900

    출력

---
+++ BaekJoon/Bronze/B5_10172_개.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze;
+
+public class B5_10172_개 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(
+                "|\\_/|\n" +
+                        "|q p|   /}\n" +
+                        "( 0 )\"\"\"\\\n" +
+                        "|\"^\"`    |\n" +
+                        "||_/=\\\\__|"
+        );
+        System.out.println(sb);
+    }
+}
