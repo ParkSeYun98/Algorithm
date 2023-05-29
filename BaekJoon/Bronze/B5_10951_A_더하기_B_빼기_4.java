2603adcc45a433f50c141661495506f8e64a5007
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-05-29 17:11:40 +0900

    for

---
+++ BaekJoon/Bronze/B5_10951_A_더하기_B_빼기_4.java
@@ -0,0 +1,26 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_10951_A_더하기_B_빼기_4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        String str;
+
+        while( (str=br.readLine()) != null ){
+
+            st = new StringTokenizer(str," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+
+        }
+        System.out.print(sb);
+    }
+}
