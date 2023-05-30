7e4e81fd0afbd9d8798f5690f79e404d34f0ef70
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-05-30 09:25:05 +0900

    s

---
+++ BaekJoon/Bronze/B5_10807_개수_세기.java
@@ -0,0 +1,31 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_10807_개수_세기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int cnt = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i : arr) {
+            if(i == T)
+                cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
