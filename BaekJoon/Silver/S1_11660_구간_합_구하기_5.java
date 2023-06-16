e7c5daa894d680dab989009c1c39393aa8525def
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-06-16 17:18:49 +0900

    2D DP

---
+++ BaekJoon/Silver/S1_11660_구간_합_구하기_5.java
@@ -0,0 +1,55 @@
+package BaekJoon.Silver;
+
+import com.sun.corba.se.impl.legacy.connection.USLPort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_11660_구간_합_구하기_5 {
+
+    static int N;
+    static int M;
+
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dp = new int[N+1][N+1];
+
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            for(int j=1; j<=N; j++)
+                dp[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<=N; i++) {
+            for(int j=2; j<=N; j++)
+                dp[i][j] += dp[i][j-1];
+        }
+
+        for(int i=2; i<=N; i++) {
+            for(int j=1; j<=N; j++)
+                dp[i][j] += dp[i-1][j];
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]);
+        }
+    }
+}
