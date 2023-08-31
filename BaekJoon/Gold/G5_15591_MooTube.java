03dbe7aad7ac90576046e23cafd827e2a6f672e4
ParkSeYun98 <yun12343@naver.com>
2023-08-31 10:02:44 +0900

    Graph / BFS

--- BaekJoon/Gold/G5_15591_MooTube.java
+++ BaekJoon/Gold/G5_15591_MooTube.java
@@ -3,16 +3,15 @@ package BaekJoon.Gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.sql.Array;
+import java.util.*;
 
 public class G5_15591_MooTube {
 
     private static int N;
     private static int Q;
 
-    private static List<Integer>[] usado;
+    private static List<Node>[] usado;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,21 +21,58 @@ public class G5_15591_MooTube {
         N = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<N; i++)
+        usado = new ArrayList[N+1];
+
+        for(int i=0; i<=N; i++)
             usado[i] = new ArrayList<>();
 
-        for(int i=0; i<N; i++) {
+        for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
 
-
+            usado[start].add(new Node(end, value));
+            usado[end].add(new Node(start, value));
         }
 
         for(int i=0; i<Q; i++) {
             st = new StringTokenizer(br.readLine(), " ");
+            int k = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            BFS(k, v);
         }
+    }
+
+    public static void BFS(int k, int v) {
+        int cnt = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+
+        boolean[] visited = new boolean[N+1];
+        visited[v] = true;
+
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+
+            List<Node> list = usado[now];
+
+            for (Node one : list) {
+                if(visited[one.node])
+                    continue;
+
+                if(one.cost < k)
+                    continue;
+
+                queue.offer(one.node);
+                visited[one.node] = true;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 
     public static class Node {
