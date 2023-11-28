ef3b82690f999a1d07a3a754995240df41804254
ParkSeYun98 <yun12343@naver.com>
2023-11-28 10:05:51 +0900

    DP

--- BaekJoon/Gold/G5_2293_동전_1.java
+++ BaekJoon/Gold/G5_2293_동전_1.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class G5_동전_1 {
+public class G5_2293_동전_1 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
