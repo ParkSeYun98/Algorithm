440f03b2312c64193974a2034b177fb80e7c8216
박세윤 <yun12343@naver.com>
2024-01-15 09:57:26 +0900

    Math

---
+++ BaekJoon/Silver/S3_25487_단순한_문제_Large.java
@@ -0,0 +1,28 @@
+package BaekJoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_25487_단순한_문제_Large {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc=0; tc<T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            sb.append(Math.min(Math.min(a, b), c)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
