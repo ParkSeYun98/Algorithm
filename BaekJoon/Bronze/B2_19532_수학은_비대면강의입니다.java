17ccc07fd521a18a96e1e68d1308f360976bc04b
ParkSeYun98 <youn12343@gmail.com>
2024-10-03 11:37:21 +0900

    BruteForce

---
+++ BaekJoon/Bronze/B2_19532_수학은_비대면강의입니다.java
@@ -0,0 +1,32 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2_19532_수학은_비대면강의입니다 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for(int i=-999; i<=999; i++) {
+            for(int j=-999; j<=999; j++) {
+                if((a*i + b*j == c) && (d*i + e*j == f)) {
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
+        }
+    }
+}
