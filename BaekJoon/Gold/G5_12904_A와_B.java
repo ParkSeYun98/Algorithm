317a484b3197eb58fae23205cbd66653ac134ebc
ParkSeYun98 <youn12343@gmail.com>
2024-10-09 16:49:31 +0900

    String

---
+++ BaekJoon/Gold/G5_12904_A와_B.java
@@ -0,0 +1,31 @@
+package BaekJoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G5_12904_A와_B {
+
+    static boolean flag;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        String T = br.readLine();
+
+        StringBuilder sbS = new StringBuilder(S);
+        StringBuilder sbT = new StringBuilder(T);
+
+        while(sbS.length() < sbT.length()) {
+            char check = sbT.charAt(sbT.length()-1);
+
+            sbT.deleteCharAt(sbT.length()-1);
+
+            if(check == 'B')
+                sbT.reverse();
+        }
+
+        System.out.println(sbS.toString().contentEquals(sbT) ? 1 : 0);
+    }
+}
