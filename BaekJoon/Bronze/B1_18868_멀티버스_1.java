d2cd22d714a9c9cb10bc7ea6cf6ae201571c8fc2
ParkSeYun98 <youn12343@gmail.com>
2024-10-30 12:31:55 +0900

    Implementation

--- BaekJoon/Bronze/B1_18868_멀티버스_1.java
+++ BaekJoon/Bronze/B1_18868_멀티버스_1.java
@@ -3,9 +3,6 @@ package BaekJoon.Bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class B1_18868_멀티버스_1 {
