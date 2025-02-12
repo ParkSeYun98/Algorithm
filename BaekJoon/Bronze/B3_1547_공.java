4698fec0da8d3c6d7e85c65c9b8d751332388113
ParkSeYun98 <youn12343@gmail.com>
2025-02-12 10:33:21 +0900

    Implementation

---
+++ BaekJoon/Bronze/B3_1547_공.java
@@ -0,0 +1,33 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B3_1547_공 {
+
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        M = Integer.parseInt(br.readLine());
+
+        int now = 1;
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == now)
+                now = b;
+            else if(b == now)
+                now = a;
+        }
+
+        System.out.println(now);
+    }
+}
