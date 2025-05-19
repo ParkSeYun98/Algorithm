a29fa3b3f21e31c617c3346157ffba7607bc28a4
ParkSeYun98 <youn12343@gmail.com>
2025-05-19 11:53:51 +0900

    Math

---
+++ BaekJoon/Bronze/B3_23971_ZOAC_4.java
@@ -0,0 +1,24 @@
+package BaekJoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B3_23971_ZOAC_4 {
+
+    static double H, W, N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        System.out.println(((int)Math.ceil(H/(N+1))) * ((int)Math.ceil(W/(M+1))));
+    }
+}
