47235c41cbb2a0a06dd96395eded30f2a775fa2a
ParkSeYun98 <youn12343@gmail.com>
2025-06-27 17:31:22 +0900

    Implementation

--- BaekJoon/Silver/S3_1485_정사각형.java
+++ BaekJoon/Silver/S3_1485_정사각형.java
@@ -1,61 +1,80 @@
 package BaekJoon.Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class S3_1485_정사각형 {
 
-    static class Point {
-        int r;
-        int c;
-
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(br.readLine());
+
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());
+        for (int i=0; i<tc; i++) {
 
-        for(int tc=0; tc<T; tc++) {
-            Point[] rectangle = new Point[4];
+            ArrayList<Point> pointList = new ArrayList<>();
 
-            for(int i=0; i<4; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int r = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
+            for (int j=0; j<4; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y =Integer.parseInt(st.nextToken());
 
-                rectangle[i] = new Point(r, c);
+                pointList.add(new Point(x, y));
             }
 
-            if(check(rectangle))
-                System.out.println(1);
+
+            double[] len = new double[6];
+            int count=0;
+
+            for (int j=0; j<3; j++) {
+                for (int k=j+1; k<4; k++) {
+                    if (j==0 && k==1)
+                        len[count++] = calculateDistance(pointList.get(j), pointList.get(k));
+                    else
+                        len[count++] = calculateDistance(pointList.get(j), pointList.get(k));
+                }
+            }
+
+            boolean isSquare = true;
+
+            Arrays.sort(len);
+
+            for (int j=1; j<4; j++)
+                if (len[0] != len[j])
+                    isSquare = false;
+
+            if (len[4] != len[5])
+                isSquare = false;
+
+            if (isSquare)
+                bw.write(1 + "\n");
             else
-                System.out.println(0);
+                bw.write(0 + "\n");
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 
-    static boolean check(Point[] rectangle) {
-        Set<Integer> set = new HashSet<>();
+    static double calculateDistance(Point point1, Point point2) {
+        int dx = point1.x - point2.x;
+        int dy = point1.y - point2.y;
 
-        for(int i=0; i<3; i++) {
-            Point A = rectangle[i];
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+}
 
-            for(int j=i+1; j<4; j++) {
-                Point B = rectangle[j];
+class Point {
+    int x;
+    int y;
 
-                set.add(Math.abs(B.r - A.r) + Math.abs(B.c - A.c));
-            }
-        }
-
-        return set.size() == 2;
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
