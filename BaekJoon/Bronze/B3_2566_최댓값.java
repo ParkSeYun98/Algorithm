d2cc10fd2d5034617fa97ab83bdc9618213d3ff9
ParkSeYun98 <youn12343@gmail.com>
2025-07-02 11:31:21 +0900

    Implementation

--- BaekJoon/Bronze/B3_2566_최댓값.java
+++ BaekJoon/Bronze/B3_2566_최댓값.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B3_2566_최댓값 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
